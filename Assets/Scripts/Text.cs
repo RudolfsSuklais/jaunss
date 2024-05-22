57f44ef792de1ccb316578a8469bcc70252ac98f
Rudolfs <ruudisrudolfs@gmail.com>
2024-05-22 15:58:25 +0300

    Pievienota poga Done

--- Assets/Scripts/Text.cs
+++ Assets/Scripts/Text.cs
@@ -1,19 +1,40 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
-using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Text : MonoBehaviour
 {
+    /*
+     // Start is called before the first frame update
+     void Start()
+     {
+         Debug.Log("Uzstartēts!"); 
+     }
+
+     public int kadrs = 0;
+     // Update is called once per frame
+     void Update()
+     {
+         kadrs++;
+         Debug.Log(kadrs);
+     }
+    */
 
     string text;
     public GameObject inputField;
     public GameObject displayField;
+    string[] words = { "Sveiki ", "Jauku dienu ", "Labrīt ", "Prieks Tevi redzēt " };
+    int randomNumb;
+
+
+    //Šeit papildināsim ar Random
     public void getText()
     {
-      
+        randomNumb = Random.Range(0, words.Length);
         text = inputField.GetComponent<Text>().text;
-        displayField.GetComponent<Text>().text = text.ToLower() + "!";
+        displayField.GetComponent<Text>().text =
+                                words[randomNumb] + text.ToLower() + "!";
     }
+
 }
