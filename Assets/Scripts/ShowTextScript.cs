a30717da636f6f892c7c7d6623897a00c3830963
Rudolfs <ruudisrudolfs@gmail.com>
2024-05-24 08:43:01 +0300

    Pielabota characteru dropdown izvele

--- Assets/Scripts/ShowTextScript.cs
+++ Assets/Scripts/ShowTextScript.cs
@@ -8,16 +8,31 @@ public class ShowTextScript : MonoBehaviour
 {
     string text;
     string year;
+    int calculatedYear;
     public GameObject inputField;
     public GameObject inputFieldYear;
     public GameObject displayField;
 
     public void getText()
     {
-       
-        text = inputField.GetComponent<Text>().text;
-        year = inputField.GetComponent<Text>().text;
-        displayField.GetComponent<Text>().text = text+year;
+  
+        text = inputField.GetComponent<InputField>().text;
+        year = inputFieldYear.GetComponent<InputField>().text;
+
+      
+        if (int.TryParse(year, out int parsedYear))
+        {
+         
+            calculatedYear = 2024 - parsedYear;
+
+            
+            displayField.GetComponent<Text>().text = $"{text}, {calculatedYear}";
+        }
+        else
+        {
+            
+            displayField.GetComponent<Text>().text = "Invalid year input.";
+        }
     }
 
 }
